a52e1bbda26f0340e192c5c042bbd9096d0f5f15
cedrictaro-g <cedric.tarou@gmail.com>
2020-12-31 15:56:18 +0900

    added docker-compose and connected to mydb

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('mysql2');
 
+const connection = db.createConnection({
+  host: 'mydb',
+  user: 'root',
+  password: 'root',
+  database: 'test_db',
+});
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
